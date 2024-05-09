1a37f32c79b2182df9e99e7053f6451b7bc1016c
momonong <morrischen2.9@gmail.com>
2024-05-09 14:32:34 +0800

    Update nginx setup with new cert some settings

--- nginx.sh
+++ nginx.sh
@@ -1,23 +1,26 @@
-nginxCopy code
 server {
     listen 80;
     listen [::]:80;
-    server_name example.com;  # 更換為你的域名或公網 IP 地址
+
+    server_name login.nckuoiachatbot.software;
+
     return 301 https://$server_name$request_uri;
 }
 
 server {
-    listen 443 ssl;
-    server_name example.com;  # 更換為你的域名或公網 IP 地址
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
 
-    ssl_certificate /etc/ssl/certs/mycertificate.crt;
-    ssl_certificate_key /etc/ssl/private/mykey.key;
-    ssl_protocols TLSv1.2 TLSv1.3;
+    server_name login.nckuoiachatbot.software;
+
+    ssl_certificate         /etc/ssl/new.pem;
+    ssl_certificate_key     /etc/ssl/key.pem;
+    ssl_protocols           TLSv1.2 TLSv1.3;
     ssl_prefer_server_ciphers on;
     ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";
 
     location / {
         include proxy_params;
-        proxy_pass http://127.0.0.1:8000;  # 確保這裡指向你的 Flask 應用
+        proxy_pass http://127.0.0.1:8000;  # 指向 Flask 應用
     }
 }
