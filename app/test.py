ff10a3d06736acf29f03039c1bf8df1c6400ca6d
momonong <morrischen2.9@gmail.com>
2024-05-28 20:16:17 +0800

    Test certs paths with test.py

---
+++ app/test.py
@@ -0,0 +1,15 @@
+import os
+
+def list_ssl_files():
+    ssl_dir = '/etc/ssl'
+    
+    # 切换到指定目录
+    os.chdir(ssl_dir)
+    
+    # 列出目录下的所有文件和子目录
+    files = os.listdir(ssl_dir)
+    for file in files:
+        print(file)
+
+if __name__ == '__main__':
+    list_ssl_files()
