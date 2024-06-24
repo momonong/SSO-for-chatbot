d12de7de383a13d2c1e60331a9ccd0f4e5fedccc
morrischen2.9 <morrischen2.9@gmail.com>
2024-06-24 03:00:47 +0000

    Update /callback router to avoid further modification with school

--- app/app.py
+++ app/app.py
@@ -108,7 +108,7 @@ def start_auth():
     return redirect(authorization_url)
 
 
-@app.route("/register/callback")
+@app.route("/callback")
 def callback():
     authorization_code = request.args.get("code")
     try:
